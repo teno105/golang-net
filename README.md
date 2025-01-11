06a89623c7f77bd9d1ac78fe5a465368256373b9
teno <teno@peppercon.co.kr>
2025-01-12 00:13:20 +0900

    채팅서버 코드 추가

--- README.md
+++ README.md
@@ -238,3 +238,73 @@ func main() {
 go run client.go
 ```
 이 코드를 통해 프로그램을 실행하면 서버에 메세지를 보내는 클라이언트가 실행됩니다.
+
+
+### 30.3 채팅 서버 제작
+
+echo 서버를 기반으로 채팅 서버를 만들겠습니다.
+채팅 서버는 연결된 모든 클라이언트들에게 모두 전송을 합니다.
+이렇게 여러 클라이언트에게 전송하는 것을 방송한다고 말하고 브로드캐스트(broadcast)라고 말하기도 합니다.
+클라이언트가 보낸 데이터를 모든 클라이언트들에게 브로드캐스트하는 채팅 서버를 만들어 보겠습니다.
+
+```go
+// cmd/server/server.go
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"sync"
+
+	gnet "github.com/panjf2000/gnet/v2"
+)
+
+type chatServer struct {
+	gnet.BuiltinEventEngine
+
+	cliMap sync.Map
+}
+
+func (cs *chatServer) OnOpen(c gnet.Conn) (out []byte, action gnet.Action) {
+	log.Printf("client connected. address:%s", c.RemoteAddr().String())
+	cs.cliMap.Store(c, true)
+	return nil, gnet.None
+}
+
+func (cs *chatServer) OnClose(c gnet.Conn, err error) (action gnet.Action) {
+	log.Printf("clinet disconnected. address:%s", c.RemoteAddr().String())
+	if _, ok := cs.cliMap.LoadAndDelete(c); ok {
+		log.Printf("connection removed")
+	}
+	return gnet.None
+}
+
+func (cs *chatServer) OnBoot(eng gnet.Engine) gnet.Action {
+	log.Printf("chat server is listening\n")
+	return gnet.None
+}
+
+func (cs *chatServer) OnTraffic(c gnet.Conn) gnet.Action {
+	buf, _ := c.Next(-1)
+	cs.cliMap.Range(func(key, value any) bool {
+		if conn, ok := key.(gnet.Conn); ok {
+			conn.AsyncWrite(buf, nil)
+		}
+		return true
+	})
+	return gnet.None
+}
+
+func main() {
+	var port int
+	var multicore bool
+
+	flag.IntVar(&port, "port", 9000, "--port 9000")
+	flag.BoolVar(&multicore, "multicore", false, "--multicore true")
+	flag.Parse()
+
+	echo := &chatServer{}
+	log.Fatal(gnet.Run(echo, fmt.Sprintf("tcp://:%d", port), gnet.WithMulticore(multicore)))
+}
+```
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,40 +4,44 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"sync"
 
 	gnet "github.com/panjf2000/gnet/v2"
 )
 
-type echoServer struct {
+type chatServer struct {
 	gnet.BuiltinEventEngine
 
-	eng       gnet.Engine
-	addr      string
-	multicore bool
+	cliMap sync.Map
 }
 
-func (es *echoServer) OnOpen(c gnet.Conn) (out []byte, action gnet.Action) {
+func (cs *chatServer) OnOpen(c gnet.Conn) (out []byte, action gnet.Action) {
 	log.Printf("client connected. address:%s", c.RemoteAddr().String())
+	cs.cliMap.Store(c, true)
 	return nil, gnet.None
 }
 
-func (es *echoServer) OnClose(c gnet.Conn, err error) (action gnet.Action) {
-	log.Printf("clinet disconnected. address:%s",
-		c.RemoteAddr().String())
+func (cs *chatServer) OnClose(c gnet.Conn, err error) (action gnet.Action) {
+	log.Printf("clinet disconnected. address:%s", c.RemoteAddr().String())
+	if _, ok := cs.cliMap.LoadAndDelete(c); ok {
+		log.Printf("connection removed")
+	}
 	return gnet.None
 }
 
-func (es *echoServer) OnBoot(eng gnet.Engine) gnet.Action {
-	es.eng = eng
-	log.Printf("echo server with multi-core=%t is listening on %s\n",
-		es.multicore, es.addr)
+func (cs *chatServer) OnBoot(eng gnet.Engine) gnet.Action {
+	log.Printf("chat server is listening\n")
 	return gnet.None
 }
 
-func (es *echoServer) OnTraffic(c gnet.Conn) gnet.Action {
+func (cs *chatServer) OnTraffic(c gnet.Conn) gnet.Action {
 	buf, _ := c.Next(-1)
-	c.Write(buf)
-	log.Printf("msg:%s", buf)
+	cs.cliMap.Range(func(key, value any) bool {
+		if conn, ok := key.(gnet.Conn); ok {
+			conn.AsyncWrite(buf, nil)
+		}
+		return true
+	})
 	return gnet.None
 }
 
@@ -49,9 +53,6 @@ func main() {
 	flag.BoolVar(&multicore, "multicore", false, "--multicore true")
 	flag.Parse()
 
-	echo := &echoServer{
-		addr:      fmt.Sprintf("tcp://:%d", port),
-		multicore: multicore,
-	}
-	log.Fatal(gnet.Run(echo, echo.addr, gnet.WithMulticore(multicore)))
+	echo := &chatServer{}
+	log.Fatal(gnet.Run(echo, fmt.Sprintf("tcp://:%d", port), gnet.WithMulticore(multicore)))
 }
